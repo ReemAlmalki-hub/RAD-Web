ea4250d0f806abfbd1962148f10ad1e669dc3a97
ReemAlmalki-hub <almalkireem@outlook.com>
2026-07-07 11:45:16 +0300

    Update excel_reader.py

--- RAD_Web/Backend/excel_reader.py
+++ RAD_Web/Backend/excel_reader.py
@@ -1,10 +1,17 @@
 import pandas as pd
+import os
+
+# Folder where excel_reader.py is located
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Go up one level (to RAD_Web) then into Data
+DATA_DIR = os.path.join(BASE_DIR, "..", "Data")
 
 
 def load_airway_dictionary():
 
     return pd.read_excel(
-        r"Data\Airway_Dictionary.xlsx",
+        os.path.join(DATA_DIR, "Airway_Dictionary.xlsx"),
         header=None
     )
 
@@ -12,6 +19,6 @@ def load_airway_dictionary():
 def load_rad_routes():
 
     return pd.read_excel(
-        r"Data\RAD_V17.1.xlsx",
+        os.path.join(DATA_DIR, "RAD_V17.1.xlsx"),
         header=1
     )
