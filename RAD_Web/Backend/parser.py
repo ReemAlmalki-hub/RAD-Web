4e61b8adff65c7f72cd1c64bb1103fb5a591e3d7
ReemAlmalki-hub <almalkireem@outlook.com>
2026-07-21 09:30:00 +0300

    Modify clean_tokens to exclude 'DCT' tokens
    Updated clean_tokens function to skip empty tokens and 'DCT'.

--- RAD_Web/Backend/parser.py
+++ RAD_Web/Backend/parser.py
@@ -5,12 +5,15 @@ def tokenize_route(route):
     return route.split()
 
 
-# Clean Tokenss
+# Clean Tokens
 def clean_tokens(tokens):
     cleaned_tokens = []
+
     for token in tokens:
         token = normalize(token)
-        if token:
+
+        # Skip empty tokens and DCT
+        if token and token != "DCT":
             cleaned_tokens.append(token)
 
     return cleaned_tokens
